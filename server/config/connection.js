918704c266142e548f3a0669257ec7e343f0197e
Syed <syedmu729@gmail.com>
2025-08-31 16:42:50 +0500

    All Set

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,32 @@
 import mongoose from "mongoose";
+import fs from "fs";
 
 const mongodbConnection = async () => {
-    const URI = process.env.DB_URI;
-    
     try {
-        await mongoose.connect(URI);
-        console.log("MongoDB Connected!!!");
+        const env = process.env.NODE_ENV;
+        
+        let URI;
+        
+        if (env === "production") {
+            const user = encodeURIComponent(process.env.DB_USER);
+            const pass = encodeURIComponent(process.env.DB_PASS);
+            const host = process.env.DB_HOST;
+            const dbName = process.env.DB_NAME;
+            
+            URI = `mongodb://${user}:${pass}@${host}:27017/${dbName}?tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false`;
+        } else {
+            // Use local MongoDB
+            const host = process.env.DB_HOST;
+            const dbName = process.env.DB_NAME;
+            
+            URI = `mongodb://${host}:27017/${dbName}`;
+        }
+        
+        await mongoose.connect(URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log(`MongoDB Connected (${env})`);
     } catch (err) {
         console.error(err.message);
         process.exit(1);
