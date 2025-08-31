fcb8d78bf21487b313dab1eaf99fc2a890d32bf2
Syed <syedmu729@gmail.com>
2025-08-31 21:49:05 +0500

    All Set

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const mongodbConnection = async () => {
     try {
-        const env = process.env.NODE_ENV;
+        const env = process.env.NODE_ENV || "development";
         let URI;
         let options = {};
         
@@ -12,15 +12,21 @@ const mongodbConnection = async () => {
             const host = process.env.DB_HOST;
             const dbName = process.env.DB_NAME;
             
+            // Absolute path to PEM file
+            const pemPath = "/home/ubuntu/certs/global-bundle.pem";
+            
             URI = `mongodb://${user}:${pass}@${host}:27017/${dbName}?replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false`;
             
             options = {
                 tls: true,
-                tlsCAFile: "/home/ubuntu/certs/global-bundle.pem",
+                tlsCAFile: pemPath,
+                tlsAllowInvalidCertificates: false, // production should validate the cert
+                serverSelectionTimeoutMS: 30000,
             };
         } else {
-            const host = process.env.DB_HOST;
-            const dbName = process.env.DB_NAME;
+            // Local MongoDB
+            const host = process.env.DB_HOST || "localhost";
+            const dbName = process.env.DB_NAME || "moodCookBook";
             URI = `mongodb://${host}:27017/${dbName}`;
         }
         
