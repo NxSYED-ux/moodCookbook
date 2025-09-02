c4be23bd5184995cd10ca99bd16904980928db99
Syed <syedmu729@gmail.com>
2025-09-02 16:53:23 +0500

    All Set.

--- client/pages/Login.tsx
+++ client/pages/Login.tsx
@@ -33,7 +33,7 @@ const Login: React.FC = () => {
     setError('');
 
     try {
-      const response = await fetch('/api/login', {
+      const response = await fetch('/api/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
